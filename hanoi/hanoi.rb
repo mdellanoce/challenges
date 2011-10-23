5f6133a313ff149ccd70747f9fb89e9c0d4ba903
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-10-23 19:06:40 -0400

    partial hanoi

---
+++ hanoi/hanoi.rb
@@ -0,0 +1,19 @@
+num_discs = 0
+num_pegs = 0
+start = []
+
+def hanoi(n, pegs)
+  if n > 0
+  end
+end
+
+ARGF.each_with_index do |line, i|
+  if i == 0
+    first = false
+    num_discs, num_pegs = line.split.map {|x| Integer(x)}
+  elsif i == 1
+    line.split.each_with_index do |disc, peg|
+             
+    end
+  end
+end
