b2668a896647d0a9f5262784cd6eb190a9de8b33
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-10-27 15:07:13 -0400

    test case for 3 disc/3 peg, with hard coded implementation

--- hanoi/graph_test.rb
+++ hanoi/graph_test.rb
@@ -40,4 +40,16 @@ class HanoiGraphTest < Test::Unit::TestCase
     moves = g.shortest_path "1", "3"
     assert_equal [[1,3]], moves
   end
+    
+  def test_three_peg_two_discs
+    g = HanoiGraph.new 2
+    moves = g.shortest_path "1 1", "2 2"
+    assert_equal [[1,3],[1,2],[3,2]], moves
+  end
+  
+  def test_three_peg_three_discs
+    g = HanoiGraph.new 3
+    moves = g.shortest_path "1 1 1", "2 2 2"
+    assert_equal [[1,2],[1,3],[2,3],[1,2],[3,1],[3,2],[1,2]], moves
+  end
 end
