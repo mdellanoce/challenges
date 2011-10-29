0e7bd3c5a1378d6915ba344ace125d29b1d56940
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-10-29 19:09:50 -0400

    passing tests

--- stringreduction/reduce.rb
+++ stringreduction/reduce.rb
@@ -1,99 +1,40 @@
-def reduce(str)
-  i = 0
-  while i < str.length-1
+def reductions(str)
+  reductions = []
+  0.upto(str.length-1) do |i|
+    r = i..i+1
+    s = str[r]
     if s == "ac" or s == "ca"
-      "b"
+      t = str.dup
+      t[r] = "b"
+      reductions.push t
     elsif s == "bc" or s == "cb"
-      "a"
+      t = str.dup
+      t[r] = "a"
+      reductions.push t
     elsif s == "ab" or s == "ba"
-      "c"
+      t = str.dup
+      t[r] = "c"
+      reductions.push t
     end
   end
-  str
+  reductions
 end
 
-class HanoiState
-  attr_reader :state
-
-  def initialize(pegs, state)
-    @pegs = pegs
-    @state = state
-    @str = state.join(" ")
-  end
+def reduce(str)
+  potentials = reductions(str)
   
-  def legal_moves
-    moves = []
-    @state.each_with_index do |source, disc|
-      1.upto(@pegs) do |destination|
-      smaller_discs = @state[0...disc]
-        under_smaller_disc = smaller_discs.count(source) > 0
-        move_to_self = destination == source
-        destination_has_smaller_disc = smaller_discs.count(destination) > 0
-
-        moves.push [source,destination] if !move_to_self and
-          !destination_has_smaller_disc and
-          !under_smaller_disc
-      end
-    end
-    moves
-  end
-  
-  def make_move(move)
-    next_state = @state.dup
-    i = next_state.find_index move[0]
-    next_state[i] = move[1]
-    HanoiState.new @pegs, next_state
-  end
-
-  def move(to)
-    @state.zip(to.state).select do |pair|
-      pair.any? {|p| pair[0] != p}
-    end.first
-  end
-
-  def to_s
-    return @str
-  end
-
-  def ==(other)
-    return to_s == other.to_s
-  end
-
-  def shortest_path(to)
-    path = {}
-    visited = {to_s => true}
-    queue = [self]
-
-    while queue.length > 0 and current = queue.pop and current != to
-      moves = current.legal_moves
-      moves.each do |m|
-        to_visit = current.make_move m
-        key = to_visit.to_s
-        if !visited[key]
-          visited[key] = true
-          queue.unshift(to_visit)
-          path[to_visit.to_s] = current
-        end
-      end
-    end
-
-    moves = []
-    rewind = to
-    while path[rewind.to_s] != nil
-      s1 = path[rewind.to_s]
-      s2 = rewind
-      moves.push(s1.move(s2))
-      rewind = s1
-    end
-    raise "Path from #{from} to #{to} not found" if moves.empty?
-    moves.reverse()
+  if potentials.any?
+    potentials.map do |reduction|
+      reduce reduction
+    end.min
+  else
+    str.length
   end
 end
 
 if __FILE__ == $0
   cases = Integer(ARGF.readline)
-  cases.time do
-    puts ARGF.readline
-    #call reduce instead
+  cases.times do
+    reduce ARGF.readline
   end
 end
--- stringreduction/reduce_test.rb
+++ stringreduction/reduce_test.rb
@@ -2,9 +2,15 @@ require 'test/unit'
 require './reduce'
 
 class ReduceTest < Test::Unit::TestCase
+  def test_reductions
+    assert_equal ["bb","cc"], reductions("cab")
+    assert_equal ["aab","bbb","bcc"], reductions("bcab")
+    assert_equal [], reductions("ccccc")
+  end
+  
   def test_reduce
-    assert_equal "cc", reduce("cab")
-    assert_equal "b", reduce("bcab")
-    assert_equal "ccccc", reduce("ccccc")
+    assert_equal 2, reduce("cab")
+    assert_equal 1, reduce("bcab")
+    assert_equal 5, reduce("ccccc")
   end
 end
