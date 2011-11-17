8486d4d612c14086b54844135d9207fbce2cc108
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-11-17 14:33:44 -0500

    brute force solution to the equations problem

--- equations/equations.rb
+++ equations/equations.rb
@@ -11,7 +11,35 @@ def multiplicity(n, p)
   m
 end
 
+class Integer
+  def factorial
+    f = 1
+    c = 1
+    while c <= self do
+      f *= c
+      c += 1
+    end
+    f
+  end
+  
+  def count_factors
+    i = 0
+    f = 1
+    while f <= self do
+      i += 1 if self % f == 0
+      f += 1
+    end
+    i
+  end
+end
+
+class Equations
+  def self.brute_force(n)
+    (n.factorial**2).count_factors
+  end
+end
+
 if __FILE__ == $0
   n = Integer(ARGF.readline)
-  #count factors of n!
+  puts Equations.brute_force(n) % 1000007
 end
