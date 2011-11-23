22c6de23af60fb860654a84f681fa7a609943eb0
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-11-23 08:50:48 -0500

    basic parsing for points ina plane problem

---
+++ pointsinaplane/pointsinaplane.rb
@@ -0,0 +1,40 @@
+class Point
+  attr_reader :x
+  attr_reader :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def slope(point)
+    Rational(point.y - @y, point.x - @x)
+  end
+end
+
+class Plane
+  def initialize
+    @points = []
+  end
+
+  def add_point(x, y)
+    @points.push Point.new(x,y)
+  end
+end
+
+if $0 == __FILE__
+  mod = 1000000007
+
+  t = Integer(ARGF.readline)
+  t.times do
+    plane = Plane.new
+
+    n = Integer(ARGF.readline)
+    n.times do
+      x,y = ARGF.readline.split.map {|a| Integer(a)}
+      plane.add_point x,y
+    end
+
+    #Figure out how many ways to remove points in minimal moves
+  end
+end
