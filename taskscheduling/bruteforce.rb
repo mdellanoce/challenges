c3a3ee2d3c5275e36cd01b2a888b0bac18bfae6b
Michael Dellanoce <michael.dellanoce@gmail.com>
2011-12-13 06:05:32 -0500

    add bruteforce solution for testing purposes

---
+++ taskscheduling/bruteforce.rb
@@ -0,0 +1,38 @@
+require 'algorithms'
+
+class Task
+  attr_reader :deadline
+  attr_reader :time
+
+  def initialize(deadline, time)
+    @deadline = deadline
+    @time = time
+  end
+
+  def <=>(other)
+    a = deadline <=> other.deadline
+    return a if a != 0
+    time <=> other.time
+  end
+end
+
+if $0 == __FILE__
+  tasks = Containers::RubyRBTreeMap.new
+
+  t = Integer(ARGF.readline)
+  t.times do
+    d,m = ARGF.readline.split.map {|i| Integer(i)}
+    
+    new_task = Task.new(d,m)
+    tasks.push new_task, new_task
+
+    time = 0
+    overshoot = 0
+    tasks.each do |task|
+      time += task.time
+      overshoot = [time - task.deadline, overshoot].max
+    end
+
+    puts overshoot
+  end
+end
